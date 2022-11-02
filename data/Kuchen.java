b4ab4b638bad78a628a7d1daf4689104e1e40aa3
Cleo <67091733+cleobfz@users.noreply.github.com>
2022-11-02 10:23:15 +0100

    Ursprungsversion von Kuchen.java

---
+++ data/Kuchen.java
@@ -0,0 +1,14 @@
+package data;
+
+/**
+ * Kuchen mit name, preis und gewicht
+ * Anzeige des Kuchens nur mit Namen und Preis
+ * 
+ * @author Cleo Schmitz
+ *
+ */
+public class Kuchen {
+	private String name;
+	private double preis;
+	private double gewicht;
+}
